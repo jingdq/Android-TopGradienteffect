ffd580b1d61abf888c3e3008814e8b0bb3601973
王龙 <wang.long@Ctrip.com>
2016-05-04 15:50:56 +0800

    Signed-off-by: 王龙 <wang.long@Ctrip.com>

--- TopGradienteffect/app/src/main/java/com/example/wanglong/topgradienteffect/HomeActivity.java
+++ TopGradienteffect/app/src/main/java/com/example/wanglong/topgradienteffect/HomeActivity.java
@@ -1,19 +1,17 @@
 package com.example.wanglong.topgradienteffect;
 
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
+import android.util.Log;
 import android.view.ViewTreeObserver;
-import android.widget.AbsListView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 
 /**
  * Created by wang.long on 2016/4/28.
  */
-public class HomeActivity extends Activity implements View.OnScrollChangeListener {
+public class HomeActivity extends Activity{
 
     private LinearLayout v;
     private ScrollView mScrollView;
@@ -39,24 +37,10 @@ public class HomeActivity extends Activity implements View.OnScrollChangeListene
     public void ChangeTitle(){
         mScrollView=(ScrollView) findViewById(R.id.scrollvisacontent);
 
-        mScrollView.setOnScrollChangeListener(this);
-
-    }
-    @Override
-    public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
-
-
-
-    }
-
-
-    //滑动title透明度变化
-    public void initChangeTitle(){
-        mScrollView=(ScrollView) findViewById(R.id.scrollvisacontent);
-        mScrollView.setOnTouchListener(new TouchListenerImpl());
-        final ViewTreeObserver.OnScrollChangedListener listener = new ViewTreeObserver.OnScrollChangedListener() {
+        mScrollView.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
             @Override
             public void onScrollChanged() {
+                Log.d("height:---------", String.valueOf(mScrollView.getScrollY()));
                 int sy=mScrollView.getScrollY();
                 if(sy>=0 && sy<=255){
                     v.getBackground().setAlpha(sy);
@@ -64,25 +48,14 @@ public class HomeActivity extends Activity implements View.OnScrollChangeListene
                     v.getBackground().setAlpha(255);
                 }
             }
-        };
-        ViewTreeObserver observer = mScrollView.getViewTreeObserver();
-        observer.addOnScrollChangedListener(listener);
+        });
     }
 
-    //监控touch事件title透明度变化
-    private class TouchListenerImpl implements View.OnTouchListener {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            //System.out.println(view.getScrollY());
-            int sy=view.getScrollY();
-            if(sy>=0 && sy<=255){
-                v.getBackground().setAlpha(view.getScrollY());
-            } else if(sy>255){
-                v.getBackground().setAlpha(255);
-            }
-            return false;
-        }
 
-    }
+
+
+
+
+
 
 }
